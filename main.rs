11348d4c6707b06e2de70da12449ade299c7d63e
gracebrien67 <104142191+gracebrien67@users.noreply.github.com>
2023-05-22 20:31:45 +0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -10,7 +10,6 @@ type DynError = Box<dyn Error>;
 
 pub mod tasks {
     use crate::docgen::{lang_features, typable_commands, write};
-    use crate::docgen::{LANG_SUPPORT_MD_OUTPUT, TYPABLE_COMMANDS_MD_OUTPUT};
     use crate::querycheck::query_check;
     use crate::themelint::{lint, lint_all};
     use crate::DynError;
@@ -38,7 +37,6 @@ pub mod tasks {
 Usage: Run with `cargo xtask <task>`, eg. `cargo xtask docgen`.
 
     Tasks:
-        docgen: Generate files to be included in the mdbook output.
         themelint <theme>: Report errors for <theme>, or all themes if no theme is specified.
         query-check: Check that tree-sitter queries are valid.
 "
